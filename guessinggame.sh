30885c1b5fb0d0028aab6d1083c4cec9d85e3541
Alicate509 <76165792+Alicate509@users.noreply.github.com>
2021-03-21 11:40:47 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -6,17 +6,13 @@ function guess(){
     do
         echo "Please, can You kindly enter your guess"
         read  number
-        if [ $number -lt $true_ans ]
+        fi [ $number -lt $true_ans ]
         then
             echo "Your guess is Less then the true number"
         elif [ $number -gt $true_ans ]
         then
             echo "Your guess is Greater then the true number"
-        else
-            echo "Congratulations! You are right!"
         break;
-        if
+        fi
     done
-}
-echo "Guess the files number in the current directory!"
-guess
+
